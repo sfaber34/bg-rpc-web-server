476357de46ddfa26e312f4fb46e2cb22141d8a41
Ubuntu <ubuntu@ip-172-31-89-145.ec2.internal>
2025-01-31 17:34:41 +0000

    fixed ssl cert paths

---
+++ config.js
@@ -0,0 +1,7 @@
+const fallbackUrl = "https://office.buidlguidl.com:48544";
+const webServerPort = 48545;
+
+module.exports = {
+  fallbackUrl,
+  webServerPort,
+};
---
+++ webServer.js
@@ -0,0 +1,36 @@
+const https = require("https");
+const express = require("express");
+const fs = require("fs");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+const app = express();
+
+const { webServerPort } = require('./config');
+
+const proxyUrlRouter = require('./routes/proxyurl');
+
+app.use(proxyUrlRouter);
+
+https.globalAgent.options.ca = require("ssl-root-cas").create(); // For sql connection
+
+app.use(bodyParser.json());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// Create the HTTPS server
+const server = https.createServer(
+  {
+    key: fs.readFileSync("/home/ubuntu/shared/server.key"),
+    cert: fs.readFileSync("/home/ubuntu/shared/server.cert"),
+  },
+  app
+);
+
+server.listen(webServerPort, () => {
+  console.log("----------------------------------------------------------------------------------------------------------------");
+  console.log("----------------------------------------------------------------------------------------------------------------");
+  console.log(`webServer.js: HTTPS server listening on port ${webServerPort}...`);
+});
